0223cba06d10ddbb74cd13d72740d515bbc7beed
TrinhHoa-23520508 <23520508@gm.uit.edu.vn>
2025-05-05 10:29:26 +0700

    hello world and disable security

--- src/main/java/com/example/sweet/SweetApplication.java
+++ src/main/java/com/example/sweet/SweetApplication.java
@@ -1,9 +1,15 @@
 package com.example.sweet;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
+//disable security
+ @SpringBootApplication(exclude = {
+ 		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+ 		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+ })
 public class SweetApplication {
 
 	public static void main(String[] args) {
