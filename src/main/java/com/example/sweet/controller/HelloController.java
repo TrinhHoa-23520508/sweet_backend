89e351b91969676a0c55515769e12b7f1290931a
TrinhHoa-23520508 <23520508@gm.uit.edu.vn>
2025-06-07 16:50:32 +0700

    config cors

--- src/main/java/com/example/sweet/controller/HelloController.java
+++ src/main/java/com/example/sweet/controller/HelloController.java
@@ -1,5 +1,9 @@
 package com.example.sweet.controller;
 
+import com.example.sweet.domain.response.RestResponse;
+import com.example.sweet.util.annotation.ApiMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/")
-    public String helloWorld(){
-        return "Hello World";
+    @ApiMessage("Hello world message")
+    public ResponseEntity<String> helloWorld(){
+        return ResponseEntity.status(HttpStatus.OK).body("Hello world");
     }
 }
---
+++ src/main/java/com/example/sweet/domain/User.java
@@ -0,0 +1,4 @@
+package com.example.sweet.domain;
+
+public class User {
+}
